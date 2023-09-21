a0ef01005968da0e12fcb16ddf8a0538dda95a29
Prateek <54174687+javaHelper@users.noreply.github.com>
2023-09-22 00:53:24 +0530

    initial

--- spring-boot-rabbitmq-example/readme.md
+++ spring-boot-rabbitmq-example/readme.md
@@ -11,3 +11,20 @@
 <img width="1454" alt="Screenshot 2023-06-02 at 5 38 56 PM" src="https://github.com/javaHelper/spring-boot-advance-demos/assets/54174687/59ac7053-1ad8-4461-84e0-aa4c423d7fd0">
 
 -----------------
+
+# Setup RabbitMQ
+
+```
+docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.10-management
+```
+
+
+```json
+curl --location 'http://localhost:7071/order/taj' \
+--header 'Content-Type: application/json' \
+--data '{
+    "name": "Pizza",
+    "qty": 10,
+    "price": 10.99
+}'
+```
--- spring-boot-rabbitmq-example/src/main/java/com/example/controller/OrderController.java
+++ spring-boot-rabbitmq-example/src/main/java/com/example/controller/OrderController.java
@@ -1,14 +1,14 @@
 package com.example.controller;
 
-import com.example.dto.Order;
-import com.example.publisher.OrderPublisher;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dto.Order;
+import com.example.publisher.OrderPublisher;
 
 @RestController
 @RequestMapping("/order")
