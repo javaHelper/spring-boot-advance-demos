23ed60c02ae0021c8120e300a938129a29bc7d9a
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-11-22 14:44:05 +0530

    Updated to Spring Boot 3.5.8

--- spring-boot-crud-mysql-demo/src/main/java/com/example/entity/Product.java
+++ spring-boot-crud-mysql-demo/src/main/java/com/example/entity/Product.java
@@ -1,15 +1,15 @@
 package com.example.entity;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Data
 @AllArgsConstructor
