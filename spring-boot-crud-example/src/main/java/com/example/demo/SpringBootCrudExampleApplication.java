e7e691c5cd1e1b9fa081c5db14a4fc9be94a2b0d
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-11-22 14:44:34 +0530

    Updated to Spring Boot 3.5.8

--- spring-boot-crud-example/src/main/java/com/example/demo/SpringBootCrudExampleApplication.java
+++ spring-boot-crud-example/src/main/java/com/example/demo/SpringBootCrudExampleApplication.java
@@ -9,5 +9,4 @@ public class SpringBootCrudExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCrudExampleApplication.class, args);
 	}
-
 }
--- spring-boot-crud-example/src/main/java/com/example/demo/entity/Product.java
+++ spring-boot-crud-example/src/main/java/com/example/demo/entity/Product.java
@@ -1,20 +1,18 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Product {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
